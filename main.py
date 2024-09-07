97f885918362f7f0bc6458113ff4065eea007f78
Draguisada <draguisada@gmail.com>
2024-09-07 15:03:38 -0300

    Feito a geração de board
    SEM BUGS!  (Eu acho)

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 
 import random
+from os import system
 
 # Criar a Board 9x9
 #Criar com números aleatórios -> Verificar quais dão problema -> remover eles -> Board Randomizada!
 def criarBoard(lado=9, altura=9):
     listaGrande = []
-    for _um in range(altura):
+    for _um in range(altura): # Cria cada linha
         listaGrande.append([])
-        for _ in range(lado):
+        for _ in range(lado): # Cria cada conteúdo na linha (0)
             # listaGrande[_um].append(random.randrange(1,9))
             listaGrande[_um].append(0)
     return listaGrande
@@ -16,6 +17,8 @@ def criarBoard(lado=9, altura=9):
 #######################
 
 def printBoard(listaGrande):
+    print("Você pode reescrever valores se achou que errou!")
+    print("0 para apagar um valor")
     quadrante1 = slice(0,3*3)
     quadrante2 = slice(3*3,6*3)
     quadrante3 = slice(6*3,9*3)
@@ -88,44 +91,45 @@ def checkLinha(listaGrande, posLetra, posNumero, valor, feedback=0) -> bool:
             return False
     
     # Checar Coluna
-    for cadaValor in listaGrande[posNumero]:
-        if cadaValor  == valor:
-            if feedback == 1: print("X - Valor já em linha")
-            return False
+    if valor in listaGrande[posNumero]:
+        if feedback == 1: print("X - Valor já em linha")
+        return False
         
-    #Fazendo qual é o quadrante do valor jogado
+    #Fazendo qual é o quadrante na linha do valor jogado
     if posLetra < 3:
         quadrante = quadrante1
-        quadrantenslide = 0
-    elif 3 < posLetra < 6:
+    elif 3 < posLetra <= 6:
         quadrante = quadrante2
-        quadrantenslide = 3
     else:
         quadrante = quadrante3
+    
+    # Quadrante na coluna
+    if posNumero < 3:
+        quadrantenslide = 0
+    elif 3 <= posNumero < 6:
+        quadrantenslide = 3
+    else:
         quadrantenslide = 6
+
     #Verifica todos os valores no quadrante que o jogador que feedback
     for quadranteVertical in range(0+quadrantenslide, 3+quadrantenslide):
-        #Pequena optimização para não contar as casas que não tenham valores
-        for casas in listaGrande[quadranteVertical][quadrante]:
-            if casas == valor:
-                if feedback == 1: print("X - Valor já em quadrante")
-                return False
-    
+        if valor in listaGrande[quadranteVertical][quadrante]:
+            if feedback == 1: print("X - Valor já em quadrante")
+            return False
+
     return True
 
-def criarValores(listaGrande, dificuldade, offset=2):
+def criarValores(listaGrande: list, dificuldade: int, offset=random.randint(0,8)):
+    offset -= 1
     
-
     # Pegar primeiro+criar uma lista de 123456789 com um offset no bloco
-    for quadra in range(1, 3): #Fazer com cada quadrante
-        print("+"*8)
-        offset+=1
-        posicao = offset
-        #offset = random.randrange(1,8)
+    for quadra in range(1, 10): #Fazer com cada quadrante
+        offset+=1 if offset < 8 else -8
+        posicao = -1
         valorAtual = 0
 
         # Pegando o quadrante atual para fazer as decisões
-        if 0 < quadra <= 3:
+        if quadra <= 3:
             quadrante = 0
         elif 3 < quadra <= 6:
             quadrante = 3
@@ -138,43 +142,40 @@ def criarValores(listaGrande, dificuldade, offset=2):
         elif quadra == 2 or quadra == 5 or quadra == 8:
             linha = range(3, 6) 
         else: 
-            linha = range(6, 9) 
+            linha = range(6, 9)
             
         ####
+        offsetCalculoInverso = offset
+        criarValoresValor = 9-offsetCalculoInverso
         for linhaAgora in linha:
-            while True:
-                # ISSO TA DANDO ERRADO, O OFFSET N TA QUEBRANDO A LINHA DIREITO
-                # SE OFFSET FOR MAIOR Q 3 FODE TUDO
-                # Possivelmente reescrever essa parte, o resto ta de boa
-                if posicao >= 3:
-                    if offset <= 3:
-                        posicao = (offset-3) *-1
+            if not criarValoresValor == 9:
+                
+                while criarValoresValor < 9:
+                    posicao += 1
+                    offsetCalculoInverso -= 1
+                    if posicao == 3: 
+                        posicao = -1
                         break
-                    posicao = (offset-3)
-                    break
-                posicaoVertical = max(((3 - offset )  // 1), 0)
-                valorAtual +=1
-                
-                
-                
-                print(f"linhaAgora {linhaAgora}")
-                print(f"posicao {posicao}")
-                print(f"valorAtual: {valorAtual}")
+                    criarValoresValor += 1
+                    listaGrande[linhaAgora][posicao+quadrante] = criarValoresValor
 
-                print(f"offset: {offset}")
-                print(f"quadra: {quadra}")
-                print("-"*6)
-
-                listaGrande[linhaAgora][posicao+quadrante] = valorAtual
-
-
-                
-                if ((posicao+quadrante+1) % 3) == 0:
-                    # listaGrande[linhaAgora][posicao+quadrante] = 0
-                    posicao = 0
-                    print("#"*6)
-                    break
+            if criarValoresValor == 9:
+                if posicao+1 == 3:
+                    posicao = -1
+                    continue
                 posicao += 1
+                while True:
+                    
+                    valorAtual +=1                    
+
+                    listaGrande[linhaAgora][posicao+quadrante] = valorAtual
+
+                    if posicao+1 == 3:
+                            posicao = -1
+                            break
+
+                    posicao += 1
+                    # Fazer sistema ao contrário slk
                 
 
                 
@@ -210,22 +211,24 @@ while jogar == 1:
 
     ### Ir para o final, ainda pra teste
     ### Achei erro que não sei corrigir. Se a pessoa pode reescrever (porque ela por errar) quer dizer que ela pode reescrever valores já colocados ali, e não sei como verificar sem usar uma gambiarra enorme e no fim não funcionar. Ix
-    print("Você pode reescrever valores se achou que errou!")
+    
     pos_valor = input("LETRA, NUMERO da casa e o valor ('casa':A1 'valor':2)" + '\n').upper().split()
     
 
-    while pos_valor[0][0].upper() not in 'ABCDEFGHI' and pos_valor[0][1] not in '123456789' and pos_valor[0] != None:
+    while pos_valor[0][0].upper() not in 'ABCDEFGHI' or pos_valor[0][1] not in '123456789':
         pos_valor[0] = input("Posição inválida")
-    while not 0 < int(pos_valor[1]) <= 9:
+    while not -1 < int(pos_valor[1]) <= 9:
         pos_valor[1] = input("Valor inválido")
     
 
     # Deixar valores em sua forma final
-    posLetra = ord(pos_valor[0][0]) - ord('A')
+    posLetra = ord(pos_valor[0][0].upper()) - ord('A')
     posNumero = int(pos_valor[0][1]) - 1
     valor = int(pos_valor[1])
 
-    if checkLinha(listaGrande, posLetra, posNumero, valor, 1):
+    
+    if valor == 0 or checkLinha(listaGrande, posLetra, posNumero, valor, 1):
+        system('cls')
         listaGrande[posNumero][posLetra] = valor
         printBoard(listaGrande)
 
@@ -233,8 +236,13 @@ while jogar == 1:
 
 
 
-    # Isso ta errado, nem funciona, mas quero ter um jeito de acabar o jogo.
-    if 0 in listaGrande:
+    naoacabar = 0
+    for linha in listaGrande:
+        if 0 in linha:
+            naoacabar = 1
+            break
+    if naoacabar == 0:
         jogar = 0
         break
-print("O jogo acabou!")
+
+input("O jogo acabou! Você ganhou!")
